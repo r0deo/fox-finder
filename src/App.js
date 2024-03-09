3827f4a8f99410631d3053f816fa21ff2e483275
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-09 16:52:35 +0300

    first shot

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useState} from 'react'
+
+ 
+function App() {
+    
+  const [likes, setLikes] = useState(0)
+   
+  const [image, setImage] = useState("https://randomfox.ca/images/41.jpg")
+  
+  function handleClick(){
+    setLikes((likes) => likes + 1)
+  }
+  
+  function handleNewFoxClick() {
+    fetch("https://randomfox.ca/floof/")
+      .then((response) => response.json())
+      .then(({ image }) => {
+        setImage(image)
+        setLikes(0)
+      })
+  }
+  
+  return (
+    <div>
+      <h1>🦊 FoxFindr 🦊</h1>
+      <div className="buttons">
+          <button onClick={handleNewFoxClick}>New 🦊 Please</button>
+          <button onClick={handleClick}>Likes: {likes}</button>
+      </div>
+      <img src={image} alt="Random Fox" />
+    </div>
+  );
+}
+
+export default App
